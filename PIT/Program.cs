6ac475f0ecc5e090fb0dfb87b3c18dedd15c0ed4
fabry <fabry>
2023-09-23 07:05:25 +0200

    0.1.0

---
+++ PIT/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIT
+{
+
+    internal class Program
+    {
+
+        private static bool _dirOnly;
+        private static bool _brokenOnly;
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Flags
+                _dirOnly = args.Contains("--dironly", StringComparer.InvariantCultureIgnoreCase);
+                _brokenOnly = args.Contains("--brokenonly", StringComparer.InvariantCultureIgnoreCase);
+
+                // Start path
+                string startPath = args.Where(x => !x.StartsWith("--")).FirstOrDefault();
+                if (startPath == null) startPath = ".";
+                startPath = Path.GetFullPath(startPath);
+
+                // Do work
+                Console.WriteLine($"PIT.exe - ver. {Assembly.GetExecutingAssembly().GetName().Version}");
+                Console.WriteLine();
+                Console.WriteLine($"Processing: {startPath}");
+                Console.WriteLine();
+                Traverse(startPath, 0);
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.ToString()}");
+            }
+        }
+
+
+        private static string OK = "-";
+        private static string KO = "X";
+
+
+        private static void Traverse(string path, int level)
+        {
+            string fill = new string(' ', level * 4);
+
+            // current directory
+            bool inheritanceEnabled = !(Directory.GetAccessControl(path).AreAccessRulesProtected);
+            if (!_brokenOnly)
+            {
+                Console.WriteLine($"{fill} {(inheritanceEnabled ? OK : KO)} {Path.GetFileName(path)}");
+            }
+            else
+            {
+                if (!inheritanceEnabled)
+                {
+                    Console.WriteLine($"{(inheritanceEnabled ? OK : KO)} {path}");
+                }
+            }
+
+            // process subdirectory
+            var subDirs = Directory.GetDirectories(path);
+            foreach (var subDir in subDirs) { Traverse(subDir, level + 1); }
+
+            // current directory files
+            if (!_dirOnly)
+            {
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    bool fileInheritanceEnabled = !(File.GetAccessControl(file).AreAccessRulesProtected);
+
+                    if (!_brokenOnly)
+                    {
+                        Console.WriteLine($"{fill}    {(fileInheritanceEnabled ? OK : KO)} {Path.GetFileName(file)}");
+                    }
+                    else
+                    {
+                        if (!fileInheritanceEnabled)
+                        {
+                            Console.WriteLine($"{(fileInheritanceEnabled ? OK : KO)} {file}");
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,9 +1,42 @@
 # PIT - Permissions Inheritance Tree
 
-PIT is a command line tool that shows the permissions inheritance status of a directory and related  files and sub-directories.
+PIT is a simple command line tool that help to discover broken permission inheritance on files and directories. It traverses the all the subdirectories showing the status of permission inheritance of files and directories.
 
-Example:  
-[TODO]
+Examples: 
+
+Problems on folder 2023\H1 and on file 2023\H2\foo.txt:
+
+```
+> PIT.exe C:\Temp\MyData
+
+Processing: C:\Temp\MyData
+
+ - MyData
+     - 2022
+         - H1
+            - bar.txt
+            - foo.txt
+         - H2
+            - bar.txt
+            - foo.txt
+     - 2023
+         X H1
+            - bar.txt
+            - foo.txt
+         - H2
+            - bar.txt
+            X foo.txt
+```
 
 
-...DRAFT...
+Same problems, in a more concise view:
+
+```
+> PIT.exe C:\Temp\MyData  --brokenOnly
+
+Processing: C:\Temp\MyData
+
+X C:\Temp\MyData\2023\H1
+X C:\Temp\MyData\2023\H2\foo.txt
+```
+
